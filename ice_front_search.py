c83239e4b25477b7852db373dff4667dc1cf6ed6
Lazy-Beee <junfan2017@gmail.com>
2021-10-25 16:18:36 -0400

    completed single-frame processing

--- ice_front_search.py
+++ ice_front_search.py
@@ -1,5 +1,7 @@
 import get_point_data as gpd
+import time
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import numpy as np
 import os.path
 
@@ -8,24 +10,36 @@ path = os.getcwd()
 """Global Variables"""
 ELEM_SIZE = 5e-4
 FILE_NAME = ['center-0300', 'node-0300']
-RUNTIME = 300
+FRAME_TIME = 300
 FINGER_ANGLE = 90 * np.pi / 180
 FINGER_VEL = 5e-6
 
 
 def finger_location(yz):
     """Find X position of hot finger surface (offset by 1 element size)"""
-    global FINGER_ANGLE, FINGER_VEL, RUNTIME
+    global FINGER_ANGLE, FINGER_VEL, FRAME_TIME
     [y, z] = yz
 
-    if (not -1.5e-3 <= y <= 5e-3) or (not -9e-3 <= z <= 0):
+    if (not -5e-3 <= y <= 5e-3) or (not -9e-3 <= z <= 0):
         print('---WARNING---YZ position outside of domain (ice_front_search: finger_location)')
         quit()
 
-    if z <= -4e-3 - ELEM_SIZE:
-        return 12e-3 + 3e-3 + RUNTIME * FINGER_VEL
+    if z <= -4.5e-3 - ELEM_SIZE or y <= -1.5e-3 - ELEM_SIZE:
+        return 30e-3
     else:
-        return (-z) / np.tan(FINGER_ANGLE / 2) + 3e-3 + RUNTIME * FINGER_VEL - ELEM_SIZE
+        return (-z) / np.tan(FINGER_ANGLE / 2) + 3e-3 + FRAME_TIME * FINGER_VEL - ELEM_SIZE
+
+
+def yz_list_generator(y_num, z_num, y_lim=(-4.5e-3, 4.5e-3), z_lim=(-8.5e-3, -0.5e-3)):
+    """Generate YZ search gird within range"""
+    y_max = min(y_lim[1], 4.5e-3)
+    y_min = max(y_lim[0], -4.5e-3)
+    z_max = min(z_lim[1], -0.5e-3)
+    z_min = max(z_lim[0], -8.5e-3)
+    y_list = [round(i * (y_max - y_min) / (y_num - 1) + y_min, 6) for i in range(y_num)]
+    z_list = [round(i * (z_max - z_min) / (z_num - 1) + z_min, 6) for i in range(z_num)]
+
+    return y_list, z_list
 
 
 def yz_search(yz, x_range=(), data_type='temp'):
@@ -54,7 +68,7 @@ def yz_search(yz, x_range=(), data_type='temp'):
                 else:
                     low = mid
             else:
-                print('---WARNING---Binary search failed (ice_front_search: yz_search_temp)')
+                print('---WARNING---Binary search failed (ice_front_search: yz_search_temp)', yz, mid_temp)
                 break
 
     elif data_type == 'lf':
@@ -69,7 +83,7 @@ def yz_search(yz, x_range=(), data_type='temp'):
                 else:
                     low = mid
             else:
-                print('---WARNING---Binary search failed (ice_front_search: yz_search_lf)')
+                print('---WARNING---Binary search failed (ice_front_search: yz_search_lf)', yz, mid_lf)
                 break
     else:
         print('---WARNING---Incorrect search data type (ice_front_search: yz_search)')
@@ -79,9 +93,11 @@ def yz_search(yz, x_range=(), data_type='temp'):
 
 
 def grid_search(y_list, z_list, data_type='temp', write=False):
-    global RUNTIME
+    global FRAME_TIME
+    start_time = time.time()
     time_step_data = {}
     i = 0
+    print('\n-----Processing Frame:', FRAME_TIME, 's.')
     for y in y_list:
         for z in z_list:
             point, data = yz_search([y, z], data_type=data_type)
@@ -89,9 +105,10 @@ def grid_search(y_list, z_list, data_type='temp', write=False):
             time_step_data[i] = {'position': point, 'liquid_fraction': data[0], 'temperature': data[1]}
 
     if write:
-        with open(os.path.dirname(path) + f'\processed_data\ice_front_{RUNTIME}.csv', 'w+') as out_file:
+        with open(os.path.dirname(path) + f'\processed_data\ice_front_{FRAME_TIME}.csv', 'w+') as out_file:
             for key in time_step_data.keys():
                 out_file.write("%s,%s\n" % (key, time_step_data[key]))
             out_file.close()
 
+    print('-----Process Time', round(time.time()-start_time, 2), 's.')
     return time_step_data
--- test_ice_front_search.py
+++ test_ice_front_search.py
@@ -1,5 +1,6 @@
 import ice_front_search as ifs
 from tqdm import trange
+import matplotlib.pyplot as plt
 
 # print('\n-----finger location test-----')
 # for i in range(9):
@@ -10,7 +11,8 @@ from tqdm import trange
 #     print(yz, ifs.finger_location(yz))
 # print('-----finger location test END-----')
 
-print('\n-----YZ search test-----')
+
+# print('\n-----YZ search test-----')
 # for i in range(7):
 #     yz = [0, -1e-3 * (i + 1)]
 #     print(yz, ifs.yz_search_temp(yz)[1])
@@ -19,18 +21,142 @@ print('\n-----YZ search test-----')
 #     yz = [-1e-3 * (i - 4), -4.5e-3]
 #     print(yz, ifs.yz_search_temp(yz))
 
-for i in range(7):
-    yz = [0, -1e-3 * (i + 1)]
-    a = ifs.yz_search(yz)
-    print('---', yz, a[0])
-    print(a[1][0])
-    print(a[1][1])
+# for i in range(7):
+#     yz = [0, -1e-3 * (i + 1)]
+#     a = ifs.yz_search(yz)
+#     print('---', yz, a[0])
+#     print(a[1][0])
+#     print(a[1][1])
+#
+# for i in range(9):
+#     yz = [-1e-3 * (i - 4), -4.5e-3]
+#     a = ifs.yz_search(yz)
+#     print('---', yz, a[0])
+#     print(a[1][0])
+#     print(a[1][1])
+# print('-----YZ search test END-----')
 
-for i in range(9):
-    yz = [-1e-3 * (i - 4), -4.5e-3]
-    a = ifs.yz_search(yz)
-    print('---', yz, a[0])
-    print(a[1][0])
-    print(a[1][1])
 
-print('-----YZ search test END-----')
+# print('\n-----List generator test-----')
+# print(ifs.yz_list_generator(11, 21))
+# print(ifs.yz_list_generator(11, 21, [2e-3, 3e-3], [-6e-3, -2e-3]))
+# print('-----List generator test END-----')
+
+
+# print('\n-----Y Plane search test-----')
+# ifs.FRAME_TIME = 300
+# ifs.FILE_NAME = ['center-0300', 'node-0300']
+# y_list, z_list = ifs.yz_list_generator(50, 50)
+# temp_y0 = ifs.grid_search([0], z_list, data_type='temp')
+# lf_y0 = ifs.grid_search([0], z_list, data_type='lf')
+#
+# temp_x, temp_z = [], []
+# lf_x, lf_z = [], []
+# for key in temp_y0:
+#     temp_x.append(temp_y0[key]['position'][0] * 1000)
+#     temp_z.append(temp_y0[key]['position'][2] * 1000)
+#     lf_x.append(lf_y0[key]['position'][0] * 1000)
+#     lf_z.append(lf_y0[key]['position'][2] * 1000)
+#     # print(temp_y0[key]['position'])
+#
+# fig, ax = plt.subplots()
+#
+# color1 = 'tab:blue'
+# color2 = 'tab:red'
+# ax.set_xlim(10, 3)
+# ax.set_xlabel('X Position (mm)')
+# ax.set_ylabel('Z Position (mm)')
+# ax.plot(temp_x, temp_z, color=color2, label='Temperature')
+# ax.plot(lf_x, lf_z, color=color1, label='Liquid Fraction')
+# ax.legend(loc="upper left")
+#
+# plt.title('90deg/323.15K hot-finger, y = 0 mm, t = 300s')
+# fig.tight_layout()
+#
+# plt.savefig('images/y0_if_temp_lf.png')
+# plt.close()
+#
+# print('-----Y Plane search test END-----')
+
+
+# print('\n-----Z Plane search test-----')
+# y_list, z_list = ifs.yz_list_generator(50, 50)
+# temp_z = ifs.grid_search(y_list, [-4.5e-3], data_type='temp')
+# lf_z = ifs.grid_search(y_list, [-4.5e-3], data_type='lf')
+#
+# temp_x, temp_y = [], []
+# lf_x, lf_y = [], []
+# for key in temp_z:
+#     temp_x.append(temp_z[key]['position'][0] * 1000)
+#     temp_y.append(temp_z[key]['position'][1] * 1000)
+#     lf_x.append(lf_z[key]['position'][0] * 1000)
+#     lf_y.append(lf_z[key]['position'][1] * 1000)
+#     # print(temp_y0[key]['position'])
+# # print(temp_x, temp_y)
+# # print(lf_x, lf_y)
+# fig, ax = plt.subplots()
+#
+# color1 = 'tab:blue'
+# color2 = 'tab:red'
+# ax.set_xlim(8.5, 6)
+# ax.set_xlabel('X Position (mm)')
+# ax.set_ylabel('Y Position (mm)')
+# ax.plot(temp_x, temp_y, color=color2, label='Temperature')
+# ax.plot(lf_x, lf_y, color=color1, label='Liquid Fraction')
+# ax.legend(loc="upper left")
+#
+# plt.title('90deg/323.15K hot-finger, z = -4.5 mm, t = 300s')
+# fig.tight_layout()
+#
+# plt.savefig('images/z-4.5_if_temp_lf.png')
+# plt.close()
+#
+# print('-----Z Plane search test END-----')
+
+
+# print('\n-----Y Plane gradient test-----')
+# n = 50
+# y_list, z_list = ifs.yz_list_generator(n, n)
+# temp_y0 = ifs.grid_search([0], z_list, data_type='temp')
+#
+# x, y, z = [], [], []
+# grad_x, grad_y, grad_z = [], [], []
+# for key in temp_y0:
+#     x.append(temp_y0[key]['position'][0] * 1000)
+#     y.append(temp_y0[key]['position'][1] * 1000)
+#     z.append(temp_y0[key]['position'][2] * 1000)
+#     grad_x.append(temp_y0[key]['temperature'][1][0])
+#     grad_y.append(temp_y0[key]['temperature'][1][1])
+#     grad_z.append(temp_y0[key]['temperature'][1][2])
+#
+# print('X position:', x)
+# print('Y position:', y)
+# print('Z position:', z)
+# print('X gradient:', grad_x)
+# print('Y gradient:', grad_y)
+# print('Z gradient:', grad_z)
+#
+# fig, ax = plt.subplots()
+#
+# color1 = 'tab:blue'
+# color2 = 'tab:red'
+# # ax.set_xlim(10, 3)
+# ax.set_xlabel('X Position (mm)')
+# ax.set_ylabel('Z Position (mm)')
+# ax.plot(x, z)
+#
+# norm_factor = 3 / max(grad_x)
+# for i in range(n):
+#     arrow_x = grad_x[i] * norm_factor
+#     arrow_z = grad_z[i] * norm_factor
+#     print(x[i], z[i], x[i] + arrow_x, z[i] + arrow_z)
+#     ax.arrow(x[i], z[i], arrow_x, arrow_z,
+#              head_width=0.1, head_length=0.2, length_includes_head=True)
+#
+# plt.title('90deg/323.15K hot-finger, y = 0 mm, t = 300s')
+# fig.tight_layout()
+#
+# plt.savefig('images/y0_if_grad.png')
+# plt.close()
+#
+# print('-----Y Plane gradient test END-----')
